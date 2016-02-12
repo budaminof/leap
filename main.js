3566c3517e4d316da88bd98b15773eda651e54ba
bud aminof <budaminof@gmail.com>
2016-02-11 21:53:46 -0700

    having tourble adding the second half of the function into the test

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 module.exports = {
   leapYear: function (year) {
-    if (year % 4 === 0) return true;
+    if (year % 100 === 0 && year % 400 !== 0){
+      return false;
+    }
+      else if (year % 4 === 0) return true;
   }
 }
--- test/test.js
+++ test/test.js
@@ -2,7 +2,10 @@ var code = require('../main.js')
 var expect = require('chai').expect
 
 describe("leapYear", function () {
+  it("will not give % by 400"), function () {
+    expect(code.leapYear(1700)).to.equal(false)
+  }
   it("will give us leap year", function() {
-    expect(code.leapYear(2016)).to.equal(true)
+  expect(code.leapYear(2016)).to.equal(true)
   })
 })
